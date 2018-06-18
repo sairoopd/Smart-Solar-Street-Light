43c0774c25f25afb0b078c9cd5d9ec526ea52c1c
sairoop dasaraju <sairoop91@gmail.com>
2018-06-17 20:19:01 -0700

    Wireless control for samrt LED using NodeMCU
    Code to detect motion using PIR sensor and to control the brightness of the LED using PWM signal. I am using a NodeMCU and blynk app to control the LED through Wi-Fi.

---
+++ final code/final_code/final_code.ino
@@ -0,0 +1,46 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "c0cf520d9aec4775957d83398156a266";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Roo";
+char pass[] = "qwertyui";
+int ledPin = 4;  // LED on Pin 13 of Arduino
+int pirPin = 7; // Input for HC-S501
+int pirValue; // Place to store read PIR Value
+
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+  pinMode(ledPin, OUTPUT);
+  pinMode(pirPin, INPUT);
+
+  digitalWrite(ledPin, LOW);
+
+}
+
+void loop()
+{
+  Blynk.run();
+  while(pirValue = LOW){
+   pirValue = digitalRead(pirPin);
+   analogWrite(ledpin, 512);
+  }
+  //Serial.println(pirValue);
+  if (pirValue = HIGH){
+   digitalWrite(ledPin, pirValue);
+  }
+}
+
